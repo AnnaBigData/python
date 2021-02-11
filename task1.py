b4222a9d7c1738b8c0b30c397f4a12f0049aecfa
AnnaBigData <78166587+AnnaBigData@users.noreply.github.com>
2021-02-11 10:57:55 +0300

    Lesson 4

---
+++ task1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 11 08:02:22 2021
+
+@author: Anna Ku
+"""
+
+def script_zp(): 
+    a = int(input("Часы: "))
+    b = int(input("Ставка: "))
+    c = int(input("Премия: "))
+    return((a*b)+c)
+print (script_zp())
---
+++ task1_1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 11 08:08:05 2021
+
+@author: Anna Ku
+"""
+
+import task1
+task1.script_zp()
+print (task1.script_zp())
+exit()
